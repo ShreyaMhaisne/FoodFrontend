0cce52ba2bb8adfd5ad4953a290dae1077319475
shreeya <pshreeya050@gmail.com>
2025-09-11 23:32:06 +0530

    Update frontend API URLs to use deployed backend

--- src/Components/Context/StoreContext.jsx
+++ src/Components/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setcartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-backendd.vercel.app/"
     const [token, setToken] = useState(localStorage.getItem("token") || "");
     const [food_list, setFoodList] = useState([])
 
